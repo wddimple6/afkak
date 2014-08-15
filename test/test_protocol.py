0cfa73c65e3bfd6c502fb40d26ee99982e346697
Robert Thille <robert.thille@cyaninc.com>
2014-08-14 20:22:38 -0700

    wip

---
+++ test/test_protocol.py
@@ -0,0 +1,25 @@
+import unittest2
+
+import afkak.protocol
+from afkak.protocol import KafkaProtocol
+from twisted.internet.error import ConnectionDone
+
+from mock import MagicMock
+
+
+class TestProtocol(unittest2.TestCase):
+    def test_stringReceived(self):
+        kp = KafkaProtocol()
+        kp.factory = MagicMock()
+        kp.stringReceived("testing")
+        kp.factory.handleResponse.assert_called_once_with("testing")
+
+    def test_connectionLost(self):
+        kp = KafkaProtocol()
+        afkak.protocol.log = MagicMock()
+        kp.factory = MagicMock()
+        kp.connectionLost()
+        self.assertIsNone(kp.factory)
+        afkak.protocol.log.error.assert_called_once_with(
+            'Lost Connection to Kafka Broker:%r',
+            ConnectionDone)
