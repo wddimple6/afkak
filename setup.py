0f9691df0e8b85b9f7d4a45c4178e4adb8b7f790
Robert Thille <rthille@ciena.com>
2016-07-08 09:29:00 -0700

    Bump version for release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="afkak",
-    version="2.2.0",
+    version="2.3.0",
 
     install_requires=['Twisted>=13.2.0'],
     extras_require={
