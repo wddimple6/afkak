d561e8e78d7b47d1b3c3a3346a90f72ab3ab073e
Robert Thille <rthille@ciena.com>
2016-11-01 12:59:20 -0700

    Fix for BPPF-330
    Overlooked when fixing BPSO-11309.  Now log only at debug level when a
    clean connection close happens, or any type of connection lost during
    shutdown.  Non-clean connection closures when not closing will still be
    logged at WARNING level.

--- afkak/test/test_protocol.py
+++ afkak/test/test_protocol.py
@@ -5,7 +5,8 @@ import unittest2
 
 import afkak.protocol
 from afkak.protocol import KafkaProtocol
-from twisted.internet.error import ConnectionDone
+from twisted.internet.error import ConnectionLost
+from twisted.python.failure import Failure
 
 from mock import MagicMock
 
@@ -17,7 +18,7 @@ class TestProtocol(unittest2.TestCase):
         kp.stringReceived("testing")
         kp.factory.handleResponse.assert_called_once_with("testing")
 
-    def test_connectionLost(self):
+    def test_connectionLost_cleanly(self):
         kp = KafkaProtocol()
         logsave = afkak.protocol.log
         try:
@@ -25,9 +26,24 @@ class TestProtocol(unittest2.TestCase):
             kp.factory = MagicMock()
             kp.connectionLost()
             self.assertIsNone(kp.factory)
+            afkak.protocol.log.debug.assert_called_once_with(
+                "Connection to Kafka Broker closed: %r Closing: %r",
+                kp.CLEAN_CLOSE, False)
+        finally:
+            afkak.protocol.log = logsave
+
+    def test_connectionLost_uncleanly(self):
+        kp = KafkaProtocol()
+        logsave = afkak.protocol.log
+        try:
+            afkak.protocol.log = MagicMock()
+            kp.factory = MagicMock()
+            failure = Failure(ConnectionLost())
+            kp.connectionLost(failure)
+            self.assertIsNone(kp.factory)
             afkak.protocol.log.warning.assert_called_once_with(
-                'Lost Connection to Kafka Broker:%r',
-                ConnectionDone)
+                'Lost Connection to Kafka Broker: %r',
+                failure)
         finally:
             afkak.protocol.log = logsave
 
@@ -40,7 +56,7 @@ class TestProtocol(unittest2.TestCase):
             kp.lengthLimitExceeded(kp.MAX_LENGTH + 1)
             kp.transport.loseConnection.assert_called_once_with()
             afkak.protocol.log.error.assert_called_once_with(
-                "KafkaProtocol Max Length:%d exceeded:%d",
+                "KafkaProtocol Max Length: %d exceeded: %d",
                 kp.MAX_LENGTH, kp.MAX_LENGTH + 1)
         finally:
             afkak.protocol.log = logsave
