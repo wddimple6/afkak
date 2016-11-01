401419ece0d5b6d689d941aae5594c2984d5a567
Robert Thille <rthille@ciena.com>
2016-11-01 12:59:20 -0700

    Fix for BPPF-330
    Overlooked when fixing BPSO-11309.  Now log only at debug level when a
    clean connection close happens, or any type of connection lost during
    shutdown.  Non-clean connection closures when not closing will still be
    logged at WARNING level.

--- afkak/protocol.py
+++ afkak/protocol.py
@@ -7,6 +7,7 @@ import logging
 
 from twisted.internet.error import ConnectionDone
 from twisted.protocols.basic import Int32StringReceiver
+from twisted.python.failure import Failure
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
@@ -22,16 +23,25 @@ class KafkaProtocol(Int32StringReceiver):
     factory = None
     closing = False  # set by factory so we know to expect connectionLost
     MAX_LENGTH = 4 * 1024 * 1024
+    CLEAN_CLOSE = Failure(ConnectionDone())
 
     def stringReceived(self, string):
         self.factory.handleResponse(string)
 
-    def connectionLost(self, reason=ConnectionDone):
-        if not (self.closing and reason.check(ConnectionDone)):
-            log.warning("Lost Connection to Kafka Broker:%r", reason)
+    def connectionLost(self, reason=CLEAN_CLOSE):
+        # If we are closing, or if the connection was cleanly closed (as
+        # Kafka brokers will do after 10 minutes of idle connection) we log
+        # only at debug level. Other connection close reasons when not
+        # shutting down will cause a warning log.
+        if self.closing or reason.check(ConnectionDone):
+            log.debug("Connection to Kafka Broker closed: %r Closing: %r",
+                      reason, self.closing)
+        else:
+            log.warning("Lost Connection to Kafka Broker: %r", reason)
+
         self.factory = None
 
     def lengthLimitExceeded(self, length):
-        log.error("KafkaProtocol Max Length:%d exceeded:%d",
+        log.error("KafkaProtocol Max Length: %d exceeded: %d",
                   self.MAX_LENGTH, length)
         self.transport.loseConnection()
