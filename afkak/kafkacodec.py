4613b19393c5594df4b5166ad6f53600d931ab45
Tom Most <tmost@ciena.com>
2017-12-27 17:41:15 -0800

    Banish xrange

--- afkak/kafkacodec.py
+++ afkak/kafkacodec.py
@@ -522,11 +522,11 @@ class KafkaCodec(object):
         ((correlation_id,), cur) = relative_unpack('>i', data, 0)
         ((num_topics,), cur) = relative_unpack('>i', data, cur)
 
-        for i in xrange(num_topics):
+        for i in range(num_topics):
             (topic, cur) = read_short_string(data, cur)
             ((num_partitions,), cur) = relative_unpack('>i', data, cur)
 
-            for i in xrange(num_partitions):
+            for i in range(num_partitions):
                 ((partition, error), cur) = relative_unpack('>ih', data, cur)
                 yield OffsetCommitResponse(topic, partition, error)
 
