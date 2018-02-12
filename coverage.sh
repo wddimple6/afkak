fe2accd889846f0413b95aa6a5d8888c9cb6cdef
Tom Most <tmost@ciena.com>
2018-02-12 15:58:00 -0800

    Add usage info to coverage.sh
    Signed-off-by: Tom Most <tmost@ciena.com>

--- coverage.sh
+++ coverage.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+# Clear coverage data, run tests, then generate a coverage report.
+#
+# Usage:
+#
+#   ./coverage.sh [KAFKA_VERSION [TOX]]
+#
+# KAFKA_VERSION - Version of Kafka to run integration tests against. See
+#                 KAFKA_ALL_VERS in the Makefile for a list of known versions.
+#                 If not provided, only unit tests will be run.
+#
+# TOX           - tox executable to use. Defaults to whatever is available on
+#                 your PATH.
 
 KAFKA_VERSION="$1"
 TOX="${2:-tox}"
