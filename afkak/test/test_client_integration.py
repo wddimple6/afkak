c6e45ca839fb36348681e7dd5d27fe53653a955a
Tom Most <tmost@ciena.com>
2018-02-15 10:51:26 -0800

    Reduce integration test message size

--- afkak/test/test_client_integration.py
+++ afkak/test/test_client_integration.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2015 Cyan, Inc.
+# Copyright 2015 Cyan, Inc.
+# Copyright 2018 Ciena Corporation
 
 import os
 import logging
@@ -34,11 +35,11 @@ log = logging.getLogger(__name__)
 class TestAfkakClientIntegration(KafkaIntegrationTestCase):
     create_client = False
 
+    if not os.environ.get('KAFKA_VERSION'):  # pragma: no cover
+        skip = 'KAFKA_VERSION is not set'
+
     @classmethod
     def setUpClass(cls):
-        if not os.environ.get('KAFKA_VERSION'):  # pragma: no cover
-            return
-
         # Single zookeeper, 3 kafka brokers
         zk_chroot = random_string(10)
         replicas = 3
@@ -62,9 +63,6 @@ class TestAfkakClientIntegration(KafkaIntegrationTestCase):
     @deferred(timeout=450)
     @inlineCallbacks
     def tearDownClass(cls):
-        if not os.environ.get('KAFKA_VERSION'):  # pragma: no cover
-            return
-
         log.debug("Closing client:%r", cls.client)
         yield cls.client.close()
         # Check for outstanding delayedCalls.
@@ -98,7 +96,7 @@ class TestAfkakClientIntegration(KafkaIntegrationTestCase):
     def test_produce_request(self):
         produce = ProduceRequest(
             self.topic, 0,
-            [create_message(self.topic + " message %d" % i)
+            [create_message(self.topic + b" message %d" % i)
              for i in range(5)])
 
         produce_resp, = yield self.client.send_produce_request([produce])
@@ -111,10 +109,14 @@ class TestAfkakClientIntegration(KafkaIntegrationTestCase):
     @deferred(timeout=5)
     @inlineCallbacks
     def test_produce_large_request(self):
+        """
+        Send large messages of about 950 KB in size. Note that per the default
+        configuration Kafka only allows up to 1 MiB messages.
+        """
         produce = ProduceRequest(
             self.topic, 0,
-            [create_message(self.topic + " message %d: " % i
-                            + "0123456789" * 1048576)
+            [create_message(self.topic + b" message %d: " % i
+                            + b"0123456789" * (950 * 100))
              for i in range(5)])
 
         produce_resp, = yield self.client.send_produce_request([produce])
@@ -179,7 +181,7 @@ class TestAfkakClientIntegration(KafkaIntegrationTestCase):
         """  # noqa
         resp = {}
         c_group = "CG_1"
-        metadata = "My_Metadata_{}".format(random_string(10))
+        metadata = "My_Metadata_{}".format(random_string(10)).encode('ascii')
         offset = random.randint(0, 1024)
         log.debug("Commiting offset: %d metadata: %s for topic: %s part: 0",
                   offset, metadata, self.topic)
