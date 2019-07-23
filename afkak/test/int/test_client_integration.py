bbd6cff2e466b24d57f4e9dc081e6b011c71c15d
Tom Most <tmost@blueplanet.com>
2019-07-23 15:22:06 -0700

    Bump both timeouts

--- afkak/test/int/test_client_integration.py
+++ afkak/test/int/test_client_integration.py
@@ -39,7 +39,7 @@ class TestAfkakClientIntegration(IntegrationMixin, unittest.TestCase):
         message_max_bytes=12 * 1048576,  # 12 MB
     )
     client_kw = dict(
-        timeout=2500,
+        timeout=30000,  # Large timeout because Travis is slow.
     )
 
     @kafka_versions("all")
@@ -112,7 +112,7 @@ class TestAfkakClientIntegration(IntegrationMixin, unittest.TestCase):
         log.debug('Sending FetchRequest')
         # Fetch request with max size of 1 MiB
         fetch = FetchRequest(self.topic, 0, 0, 1024 ** 2)
-        fetch_resp, = yield self.client.send_fetch_request([fetch], max_wait_time=10000)
+        [fetch_resp] = yield self.client.send_fetch_request([fetch], max_wait_time=10000)
         log.debug('Got FetchResponse %r', fetch_resp)
         self.assertEqual(fetch_resp.error, 0)
         self.assertEqual(fetch_resp.topic, self.topic)
