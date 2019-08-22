9468ad19fbf812b51282e0b382460b2d650410b6
Tom Most <tmost@blueplanet.com>
2019-08-21 18:36:21 -0700

    Prepare for release
    Signed-off-by: Tom Most <tmost@blueplanet.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 # NB: This version is extracted by the Makefile using awk; don't change the
 # formatting here!
-version = "19.8.0b1"
+version = "19.8.0b2"
 
 with open('README.md', 'r') as fin:
     readme_lines = fin.readlines()
